ad151499785da8a77b9cce395d2bd18b1a1c5581
Muhammad Abuzar <syed.abuzar.badar@gmail.com>
2023-05-25 17:41:00 +0400

    added routes for post component

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,7 +3,13 @@ import express from 'express';
 import { signupUser, loginUser } from '../controller/user-controller.js';
 import { uploadImage, getImage } from '../controller/image-controller.js';
 import upload from '../utils/upload.js';
-import { createPost } from '../controller/post-controller.js';
+import {
+  createPost,
+  deletePost,
+  getAllPosts,
+  getPost,
+  updatePost,
+} from '../controller/post-controller.js';
 import { authenticateToken } from '../controller/jwt-controller.js';
 
 const router = express.Router();
@@ -13,5 +19,8 @@ router.post('/file/upload', upload.single('file'), uploadImage);
 router.get('/file/:filename', getImage);
 
 router.post('/create', authenticateToken, createPost);
-
+router.get('/posts', authenticateToken, getAllPosts);
+router.put('/update/:id', authenticateToken, updatePost);
+router.delete('/delete/:id', authenticateToken, deletePost);
+router.get('/post/:id', authenticateToken, getPost);
 export default router;
